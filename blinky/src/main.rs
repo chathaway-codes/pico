0fd1acb01eb70baf487f2921706df97d90e5d3ab
Charles Hathaway <chathaway@logrit.com>
2022-06-19 16:14:48 -0700

    add: very basic program that compiles

---
+++ blinky/src/main.rs
@@ -0,0 +1,14 @@
+#![no_std]
+#![no_main]
+
+// The macro for our start-up function
+use cortex_m_rt::entry;
+
+// Ensure we halt the program on panic (if we don't mention this crate it won't
+// be linked)
+use panic_halt as _;
+
+#[entry]
+fn main() -> ! {
+    loop {}
+}
