aab10fdd867c998ae7129633f756255b70698650
Charles Hathaway <chathaway@logrit.com>
2022-06-20 08:29:01 -0700

    add: mvp of a blinking light

--- blinky/src/main.rs
+++ blinky/src/main.rs
@@ -1,14 +1,73 @@
 #![no_std]
 #![no_main]
 
+#[cfg(not(test))]
+use core::panic::PanicInfo;
+
+// peripheral access crate
+use rp_pico::{
+    self,
+    hal::{self, pac, prelude::*},
+};
+
+// Time handling traits
+use embedded_time::rate::*;
+
+// GPIO traits
+use embedded_hal::digital::v2::OutputPin;
+
 // The macro for our start-up function
 use cortex_m_rt::entry;
 
-// Ensure we halt the program on panic (if we don't mention this crate it won't
-// be linked)
-use panic_halt as _;
-
 #[entry]
 fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+
+    // Watchdog is a HW timer that counts down, and if it
+    // reaches 0, restarts the program. We use this as a time
+    // when creating our clock.
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    // Configure the clocks
+    //
+    // The default is to generate a 125 MHz system clock
+    let clocks = hal::clocks::init_clocks_and_plls(
+        rp_pico::XOSC_CRYSTAL_FREQ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    // create the delay
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
+
+    // get access to the GPIO pins
+    let sio = hal::Sio::new(pac.SIO);
+    let pins = rp_pico::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    // get the LED for writing
+    let mut led_pin = pins.led.into_push_pull_output();
+    loop {
+        led_pin.set_high().unwrap();
+        delay.delay_ms(500);
+        led_pin.set_low().unwrap();
+        delay.delay_ms(500);
+    }
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_: &PanicInfo) -> ! {
     loop {}
 }
